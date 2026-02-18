cb793b1f76f5a926e816d32d0c3b9eef506bc3be
lorischock <110647084+lorischock@users.noreply.github.com>
2026-02-18 13:01:13 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
+from urllib.parse import urljoin
 
 URL = "https://www.estepona.es/actualidad/"
 
-response = requests.get(URL)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(URL, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
 fg = FeedGenerator()
@@ -14,9 +19,16 @@ fg.description("Latest news from Ayuntamiento de Estepona")
 
 articles = soup.select("h2 a")
 
-for article in articles[:10]:
+for article in articles:
+    href = article.get("href")
+
+    if not href:
+        continue
+
+    full_url = urljoin(URL, href)
+
     fe = fg.add_entry()
     fe.title(article.get_text(strip=True))
-    fe.link(href=article["href"])
+    fe.link(href=full_url)
 
 fg.rss_file("rss.xml")
